dc7b94339fbc7d547734468a24ac52000adcabe4
niks-nikhil-anand <niks.anand.developer@gmail.com>
2026-06-24 17:06:03 +0530

    refactor: consolidate timeline components into FlowTimeline and clean up unused files

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useState, useEffect } from "react";
+import FlowTimeline from "./components/FlowTimeline";
 
 export default function Home() {
   const [loaded, setLoaded] = useState(false);
@@ -11,16 +12,7 @@ export default function Home() {
 
   return (
     <main className="relative w-screen h-screen overflow-hidden bg-[#12121a]">
-      {/* Standalone Rubenius Timeline App Iframe */}
-      {loaded && (
-        <iframe
-          src="/goghwiththeflow/index.html"
-          className="w-full h-full border-0 select-none"
-          title="Rubenius Interiors - Twenty Years of Interior Wellbeing"
-          allow="autoplay; fullscreen; encrypted-media; picture-in-picture"
-          sandbox="allow-scripts allow-same-origin allow-downloads allow-forms"
-        />
-      )}
+      {loaded && <FlowTimeline />}
     </main>
   );
 }
